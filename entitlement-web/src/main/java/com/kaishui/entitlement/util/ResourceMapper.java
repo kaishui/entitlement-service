1f89e93cde52cea99b2155b4d053af819d9a57d8
kaishui <yiqiesuifengf@163.com>
2025-05-08 18:37:16 +0800

    feat: add Entitlement to user

--- entitlement-web/src/main/java/com/kaishui/entitlement/util/ResourceMapper.java
+++ entitlement-web/src/main/java/com/kaishui/entitlement/util/ResourceMapper.java
@@ -5,23 +5,34 @@ import com.kaishui.entitlement.entity.dto.CreateResourceDto;
 import com.kaishui.entitlement.entity.dto.ResourceDto;
 import com.kaishui.entitlement.entity.dto.UpdateResourceDto;
 import com.kaishui.entitlement.entity.Resource;
+import org.bson.Document;
 import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", // Integrates with Spring DI
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, // Ignore nulls during update
-        unmappedTargetPolicy = ReportingPolicy.IGNORE) // Ignore unmapped fields
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ResourceMapper {
 
-    // --- Entity to DTO ---
     ResourceDto toDto(Resource resource);
 
-    // --- DTO to Entity ---
+    // Let MapStruct do the initial mapping, ignore permission for now
+    @Mapping(target = "permission", ignore = true)
     Resource toEntity(CreateResourceDto createDto);
 
-    // --- Update Entity from DTO ---
-    // This method updates the 'target' entity with non-null values from the 'source' DTO
+    @AfterMapping
+    default void afterMappingCreateDtoToEntity(CreateResourceDto dto, @MappingTarget Resource entity) {
+        if (dto.getPermission() != null) {
+            entity.setPermission(new Document(dto.getPermission())); // Deep copy
+        }
+    }
+
     void updateEntityFromDto(UpdateResourceDto source, @MappingTarget Resource target);
 
-    // If you need to map UpdateResourceDto directly to a new Resource entity (less common for updates)
-    // Resource toEntity(UpdateResourceDto updateDto);
+    // Similar @AfterMapping might be needed for updateEntityFromDto if it handles 'permission'
+    @AfterMapping
+    default void afterMappingUpdateDtoToEntity(UpdateResourceDto dto, @MappingTarget Resource entity) {
+        if (dto.getPermission() != null) { // Assuming UpdateResourceDto also has a Document permission
+            entity.setPermission(new Document(dto.getPermission())); // Deep copy
+        }
+    }
 }
